969b7907471db287298fda51241bb5ce2494dd12
ちょ <choskiyo@gmail.com>
2019-02-22 00:15:54 +0000

    rm unused func

--- auth/auth.go
+++ auth/auth.go
@@ -1,34 +1,11 @@
 package auth
 
 import (
-	"net/http"
-
 	"github.com/choskyo/blog/data"
 	"github.com/choskyo/blog/users"
 	"github.com/gin-gonic/gin"
-	"github.com/wader/gormstore"
 	"golang.org/x/crypto/bcrypt"
 )
-
-func isAuthorized(Store *gormstore.Store) gin.HandlerFunc {
-	return func(c *gin.Context) {
-
-		sessionValues, err := Store.Get(c.Request, "connect.s.id")
-
-		if err != nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"message": "You are not authorized to view this."})
-			c.Abort()
-		}
-
-		if sessionValues.Values["Authorised"] != true {
-			c.JSON(http.StatusUnauthorized, gin.H{"message": "You are not authorized to view this."})
-			c.Abort()
-		}
-
-		// Pass the user id into the handler.
-		c.Set("userId", sessionValues.Values["userId"])
-	}
-}
 
 // Setup initalises routes + tables
 func Setup(r *gin.Engine) {
