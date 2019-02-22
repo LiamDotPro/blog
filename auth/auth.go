20911421f2d5bb8eb6e9afd3ac98ce61e9f399cc
ちょ <choskiyo@gmail.com>
2019-02-22 19:47:48 +0000

    wip

--- auth/auth.go
+++ auth/auth.go
@@ -1,6 +1,10 @@
 package auth
 
 import (
+	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/choskyo/blog/data"
 	"github.com/choskyo/blog/users"
 	"github.com/gin-gonic/gin"
@@ -10,6 +14,7 @@ import (
 // Setup initalises routes + tables
 func Setup(r *gin.Engine) {
 	r.POST("/api/auth/login", login)
+	r.GET("/api/auth/test", testLogin)
 }
 
 func login(c *gin.Context) {
@@ -32,11 +37,48 @@ func login(c *gin.Context) {
 		return
 	}
 
-	session, _ := data.Store.Get(c.Request, "session")
-	session.Values["userID"] = user.ID
-	session.Save(c.Request, c.Writer)
+	// Setup new session.
+	session, err := data.Store.New(c.Request, "session")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	session.Values["Authorised"] = true
+	session.Values["userId"] = user.ID
+
+	data.Store.Save(c.Request, c.Writer, session)
+
+	c.JSON(http.StatusOK, gin.H{
+		"attempt": "uh ok",
+		"message": "You have successfully logged into your account.",
+	})
+}
+
+func testLogin(c *gin.Context) {
+	session, err := data.Store.Get(c.Request, "session")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v", err)
+	}
+
+	val, ok := session.Values["userId"]
+
+	fmt.Printf("%v\n\n", len(session.Values))
+
+	if !ok {
+		fmt.Fprintf(os.Stderr, "not ok!\n")
+		c.Status(500)
+		return
+	}
+
+	username, ok := val.(string)
+	if !ok {
+		fmt.Fprintf(os.Stderr, "casting was not ok!\n")
+		c.Status(500)
+		return
+	}
 
 	c.JSON(200, gin.H{
-		"message": "Success",
+		"user": username,
 	})
 }
